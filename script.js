ee6c0dc90b293988e7aeaa833f51f50b8b55e2fb
Bechera <becherachapman@gmail.com>
2022-05-09 08:56:42 -0400

    added animations

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
 const phrases = [
     'Creative',
     'Passionate',
-    'Goal Oriented',
+    'Goal-oriented',
     'Always Learning',
+    'Frontend',
+    'Driven',
+    'Backend',
+    'Positive',
+    'Web Design',
+    'Detail-Oriented',
+    'Self-Motivated',
+    'Valuable',
+    'Leader',
+    'Energized',
+    'Accomplished',
+    'Resolved'
 ]
 
-const titleChanger = () => {
-    let titleChange = document.getElementById('title')
-    console.log(titleChange)
-    
-    setInterval(() => {
-        let ramdomNum = Math.floor(Math.random() * 4);
-        titleChange.innerHTML = phrases[ramdomNum]
-    }, 10000)
-}
+const sectionOne = document.querySelector('#AboutMe');
 
-titleChanger();
+const options = {
+  root: null, // it is the viewport
+  threshold: 0,
+  rootMargin: "-150px",
+ };
+
+
+
+const observer = new IntersectionObserver((entries, observer) => {
+entries.forEach(entry => {
+    console.log(entry.isIntersecting)
+
+});
+}, options);
+
+observer.observe(sectionOne)
+
+//  (() => {
+//     let titleChange = document.getElementById('profile-container')
+//     console.log(titleChange)
+    
+//     setInterval(() => {
+//         let ramdomNum = Math.floor(Math.random() * phrases.length);
+//         titleChange.innerHTML = phrases[ramdomNum]
+//     }, 10000)
+//  })
+
